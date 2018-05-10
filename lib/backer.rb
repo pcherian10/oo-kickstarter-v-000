f5677ed0c43c2304b49e34c678c04f344f8f6aac
Paul <pcherian10@gmail.com>
2018-05-10 20:12:47 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ class Backer
 
   def back_project (project)
     @backed_projects << project unless @backed_projects.include?(project)
-    project.add_backer(self) unless
+    project.add_backer(self)
   end
 
 end
