26a800bbdad7ed0e6aedeb4e87fd08499016037b
Paul <pcherian10@gmail.com>
2018-05-10 19:50:17 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Project
+
+  attr_accessor :title, :backers
+
+  def initialize (title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer (backer)
+    @backers << backer if !@backers.include?(backer)
+    backer.back_project(self)
+
+  end
+
+end
